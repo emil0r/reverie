7497972da9898365d08f4f08e384729984f44f0f
Emil Bengtsson <emil@emil0r.com>
2012-11-28 17:44:48 +0100

    initial commit

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject reverie "0.1.0-SNAPSHOT"
+  :description "A sane CMS"
+  :url "http://reveriecms.org"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.4.0"]])
---
+++ test/reverie/core_test.clj
@@ -0,0 +1,7 @@
+(ns reverie.core-test
+  (:use clojure.test
+        reverie.core))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
