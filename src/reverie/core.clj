1f019e905a84f515448d2574911788e693dc424a
Emil Bengtsson <emil@emil0r.com>
2012-12-09 19:12:24 +0100

    deftemplate added

--- src/reverie/core.clj
+++ src/reverie/core.clj
@@ -1,2 +1,20 @@
 (ns reverie.core)
 
+(defonce routes (atom {}))
+(defonce templates (atom {}))
+(defonce objects (atom {}))
+(defonce apps (atom {}))
+
+
+
+(defmacro deftemplate [template args body]
+  (let [template (keyword template)
+        options (loop [m {}
+                       [opt & options] (partition 2 args)]
+                  (if (nil? opt)
+                    m
+                    (let [[k v] opt]
+                      (recur (assoc m k v) options))))]
+    `(swap! routes assoc ~template {:options ~options
+                                    :fn (fn [~'request] ~body)})))
+
--- test/reverie/test/core.clj
+++ test/reverie/test/core.clj
@@ -1,1 +1,33 @@
-(ns reverie.test.core)
+(ns reverie.test.core
+  (:require [reverie.core :as rev])
+  (:use midje.sweet))
+
+
+(defn pre-test [request]
+  (keys request))
+
+(defn post-test [request]
+  (keys request))
+
+
+(defn reset-routes! []
+  (reset! rev/routes {}))
+
+(defn request []
+  {:uri "/"})
+
+
+(fact
+ "deftemplate"
+ (do
+   (reset-routes!)
+   (rev/deftemplate :main [:areas [:a :b :c] :pre [pre-test] :post [post-test]] "body")
+   (let [m @rev/routes
+         k (first (keys m))
+         options (:options (m k))
+         func (:fn (m k))]
+     [k options (apply func (request))])) => [:main
+                                              {:areas [:a :b :c]
+                                               :pre [pre-test]
+                                               :post [post-test]}
+                                              "body"])
