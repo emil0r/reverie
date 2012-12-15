60d23ab109a330cae6bce47e3887b37a9dc4c61a
Emil Bengtsson <emil@emil0r.com>
2012-12-15 17:28:40 +0100

    can now have mulitple method/fn  defined in a defobject

--- src/reverie/core.clj
+++ src/reverie/core.clj
@@ -26,26 +26,35 @@
     `(swap! routes assoc ~template {:options ~options
                                     :fn (fn [~'request] ~body)})))
 
-
-
-
 (defmacro object-funcs [attributes methods & body]
-  `(let [~'func (fn [~'request ~@attributes] ~@body)]
-     (into {} (map vector ~methods (repeat ~'func)))))
+  (let [all-kw? (zero? (count (filter #(not (keyword? %)) methods)))]
+    (if all-kw?
+      `(let [~'func (fn [~'request ~@attributes] ~@body)]
+         (into {} (map vector ~methods (repeat ~'func))))
+      
+      (let [paired (into {} (map (fn [[method fn-name]] {(keyword fn-name) method }) (partition 2 methods)))
+            bodies (map (fn [[fn-name & fn-body]] [(keyword fn-name) fn-body]) (filter vector? body))]
+        (loop [[func-vector & r] bodies
+               m {}]
+          (if (nil? func-vector)
+            m
+            (let [[fn-name fn-body] func-vector]
+             (if-let [method (paired (first func-vector))]
+               (recur r (assoc m method `(fn [~'request] ~@fn-body)))
+               (recur r m)))))))))
 
 (macroexpand-1 (object-funcs [] [:get] (html5 request "body")))
+(macroexpand-1 (object-funcs [] [:get fn-get :post fn-post] [fn-get "get function"] [fn-post "post function"]))
+((:post (object-funcs [] [:get fn-get :post fn-post] [fn-get (html5 "get function" "yeah?" request)] [fn-post "post function"])) {:uri "/"})
 (:get (object-funcs [] [:get] (html5 request)))
-((:get (object-funcs [x y] [:get] (html5 request))) {:uri "/"} "my x" "my y")
-x((:get (object-funcs [] [:get :post] (html5 [:div "my body"]) )) {})
+((:get (object-funcs [x y] [:get] (html5 request x y))) {:uri "/"} "my x" "my y")
+((:get (object-funcs [] [:get :post] (html5 [:div "my body"]) )) {})
 
 (defmacro defobject [object options methods & args]
   (let [object (keyword object)
         options (parse-options options)
         `~body `(object-funcs [] ~methods ~@args)]
-    `(do
-       {~object {:options ~options
-                 :fns ~body
-                 }})))
+    `{~object (merge {:options ~options} ~body)}))
 
 
 (macroexpand-1 (defobject text [:areas [:a :b] :schema "asdf.clj"] [:get] "body"))
@@ -53,6 +62,6 @@ x((:get (object-funcs [] [:get :post] (html5 [:div "my body"]) )) {})
 (macroexpand-1 (defobject text [:areas [:a :b] :schema "asdf.clj"] [:any] "body"))
 (macroexpand-1 (defobject text [:areas [:a :b] :schema "asdf.clj"] [:any] (html5 request "body")))
 
-
-((:get (:fns (:text (defobject text [:areas [:a :b]] [:get] (html5 request "body"))))) {:uri "/"})
-(-> (defobject text [:areas [:a :b] :schema "asdf.clj"] [:get] (html5 "body")) :text :fns :get)
+(:text (defobject text [:areas [:a :b]] [:get] (html5 request "body")))
+((:get (:text (defobject text [:areas [:a :b]] [:get] (html5 request "body")))) {:uri "/"})
+(-> (defobject text [:areas [:a :b] :schema "asdf.clj"] [:get] (html5 "body")) :text :get)
