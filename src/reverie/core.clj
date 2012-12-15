22c2fa12d612bb417475e34d591e6062951aba63
Emil Bengtsson <emil@emil0r.com>
2012-12-15 20:08:45 +0100

    clean up a bit and added tests for object-funcs

--- src/reverie/core.clj
+++ src/reverie/core.clj
@@ -31,7 +31,6 @@
     (if all-kw?
       `(let [~'func (fn [~'request ~@attributes] ~@body)]
          (into {} (map vector ~methods (repeat ~'func))))
-      
       (let [paired (into {} (map (fn [[method fn-name]] {(keyword fn-name) method }) (partition 2 methods)))
             bodies (map (fn [[fn-name & fn-body]] [(keyword fn-name) fn-body]) (filter vector? body))]
         (loop [[func-vector & r] bodies
@@ -40,28 +39,11 @@
             m
             (let [[fn-name fn-body] func-vector]
              (if-let [method (paired (first func-vector))]
-               (recur r (assoc m method `(fn [~'request] ~@fn-body)))
+               (recur r (assoc m method `(fn [~'request ~@attributes] ~@fn-body)))
                (recur r m)))))))))
-
-(macroexpand-1 (object-funcs [] [:get] (html5 request "body")))
-(macroexpand-1 (object-funcs [] [:get fn-get :post fn-post] [fn-get "get function"] [fn-post "post function"]))
-((:post (object-funcs [] [:get fn-get :post fn-post] [fn-get (html5 "get function" "yeah?" request)] [fn-post "post function"])) {:uri "/"})
-(:get (object-funcs [] [:get] (html5 request)))
-((:get (object-funcs [x y] [:get] (html5 request x y))) {:uri "/"} "my x" "my y")
-((:get (object-funcs [] [:get :post] (html5 [:div "my body"]) )) {})
 
 (defmacro defobject [object options methods & args]
   (let [object (keyword object)
         options (parse-options options)
         `~body `(object-funcs [] ~methods ~@args)]
     `{~object (merge {:options ~options} ~body)}))
-
-
-(macroexpand-1 (defobject text [:areas [:a :b] :schema "asdf.clj"] [:get] "body"))
-(macroexpand-1 (defobject text [:areas [:a :b] :schema "asdf.clj"] [:get] (html5 "body")))
-(macroexpand-1 (defobject text [:areas [:a :b] :schema "asdf.clj"] [:any] "body"))
-(macroexpand-1 (defobject text [:areas [:a :b] :schema "asdf.clj"] [:any] (html5 request "body")))
-
-(:text (defobject text [:areas [:a :b]] [:get] (html5 request "body")))
-((:get (:text (defobject text [:areas [:a :b]] [:get] (html5 request "body")))) {:uri "/"})
-(-> (defobject text [:areas [:a :b] :schema "asdf.clj"] [:get] (html5 "body")) :text :get)
--- test/reverie/test/core.clj
+++ test/reverie/test/core.clj
@@ -31,3 +31,21 @@
                                                :pre [pre-test]
                                                :post [post-test]}
                                               "body"])
+
+
+(fact
+ "objectfuncs simple"
+ (let [obj (rev/object-funcs [] [:get :post] (clojure.string/join " " ["this" "is" "my" "function!"]))]
+   [((:get obj) {}) ((:post obj) {})]) => ["this is my function!" "this is my function!"])
+
+(fact
+ "objectfuncs multiple method/fn"
+ (let [obj (rev/object-funcs [] [:get fn-get :post fn-post]
+                             [fn-get "my get"]
+                             [fn-post "my post"])]
+   [((:get obj) {}) ((:post obj) {})]) => ["my get" "my post"])
+
+(fact
+ "objectfuncs attributes"
+ (let [obj (rev/object-funcs [text] [:get] (str "this is my " text))]
+   ((:get obj) {} "text")) => "this is my text")
