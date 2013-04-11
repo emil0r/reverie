3f7e08a6d9c1fb31ec9e62a10f7e0e6b314f0101
Emil Bengtsson <emil@emil0r.com>
2013-04-11 20:26:21 +0200

    added slingshot as dependancy

--- project.clj
+++ project.clj
@@ -7,7 +7,8 @@
                  [ring "1.1.6"]
                  [com.datomic/datomic-free "0.8.3848"]
                  [fs "1.3.2"]
-                 [clout "1.1.0"]]
+                 [clout "1.1.0"]
+                 [slingshot "0.10.3"]]
   :profiles {:dev {:dependencies [[midje "1.4.0"]
                                   [com.stuartsierra/lazytest "1.2.3"]
                                   [ring-mock "0.1.3"]]}}
