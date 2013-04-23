6f5eaa30d60bed045a29192d01101226a16c64cb
Emil Bengtsson <emil@emil0r.com>
2013-04-23 08:00:28 +0200

    added load-views and load-views-ns. added checks to server/start

--- src/reverie/middleware.clj
+++ src/reverie/middleware.clj
@@ -1,7 +1,10 @@
 (ns reverie.middleware
   (:require [ring.middleware.reload :as r-reload]))
 
+(defn dev-mode? [options]
+  (= (:mode options) :dev))
+
 (defn reload [handler options]
-  (if (:dev-mode? options)
+  (if (dev-mode? options)
     (r-reload/wrap-reload handler options))
   handler)
--- src/reverie/server.clj
+++ src/reverie/server.clj
@@ -1,7 +1,18 @@
 (ns reverie.server
-  (:use [datomic.api :only [db tempid]])
+  (:use [bultitude.core :only [namespaces-on-classpath]]
+        [clojure.java.io :only [file]]
+        [datomic.api :only [db tempid]])
   (:require [reverie.core :as rev]
             [reverie.middleware :as middleware]))
+
+(defn load-views [& dirs]
+  (doseq [f (namespaces-on-classpath :classpath (map file dirs))]
+    (require f)))
+
+(defn load-views-ns [& ns-syms]
+  (doseq [sym ns-syms
+          f (namespaces-on-classpath :prefix (name sym))]
+    (require f)))
 
 (defn generate-handler [get-connection handlers {:keys [dev-mode?] :as options}]
   (reduce (fn [current [handler & args]]
@@ -16,15 +27,19 @@
 
 (defmulti start :database)
 (defmethod start :default [{:keys [port get-connection handlers] :as options}]
-  (require 'ring.adapter.jetty)
-  (println "Starting server... ")
-  (let [jetty-options (merge {:port port :join? false :dev-mode? true}
-                             (:jetty-options options))
-        run-fn (resolve 'ring.adapter.jetty/run-jetty)]
-    (swap! rev/settings options)
-    (rev/run-schemas! (get-connection))
-    (println (str "Server started on port " port "."))
-    (run-fn (generate-handler get-connection handlers options) jetty-options)))
+  (cond
+      (nil? port) (println "No port specified.")
+      (nil? get-connection) (println "No get-connection specified.")
+      :else (do
+              (require 'ring.adapter.jetty)
+              (println "Starting server... ")
+              (let [jetty-options (merge {:port port :join? false :mode :dev}
+                                         (:jetty-options options))
+                    run-fn (resolve 'ring.adapter.jetty/run-jetty)]
+                (swap! rev/settings options)
+                (rev/run-schemas! (get-connection))
+                (println (str "Server started on port " port "."))
+                (run-fn (generate-handler get-connection handlers options) jetty-options)))))
 
 (defn stop [server]
   (println "Stopping server...")
