b24e973e9d8b13201505d8e3cfbce2ec8386f26b
Emil Bengtsson <emil@emil0r.com>
2013-08-12 11:49:31 +0200

    moving in direction of SQL

--- project.clj
+++ project.clj
@@ -5,12 +5,14 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [ring "1.1.8"]
-                 [com.datomic/datomic-free "0.8.3848"]
+                 [korma "0.3.0-RC5"]
                  [fs "1.3.2"]
                  [clout "1.1.0"]
                  [slingshot "0.10.3"]
-                 [bultitude "0.1.5"]]
-  :profiles {:dev {:dependencies [[midje "1.6-alpha1"]
+                 [bultitude "0.1.5"]
+                 [lobos "1.0.0-beta1"]]
+  :profiles {:dev {:dependencies [[com.datomic/datomic-free "0.8.3848"]
+                                  [midje "1.6-alpha1"]
                                   [com.stuartsierra/lazytest "1.2.3"]
                                   [ring-mock "0.1.3"]]}}
   :repositories {"stuart" "http://stuartsierra.com/maven2"})
