b032de507796cf8135b13fea658ea0b91cd35903
Emil Bengtsson <emil@emil0r.com>
2013-08-23 00:18:04 +0200

    updated project.clj

--- project.clj
+++ project.clj
@@ -5,6 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [ring "1.2.0"]
+                 ;;[ring-anti-forgery "0.2.1"]
                  [korma "0.3.0-RC5"]
                  [me.raynes/fs "1.4.5"]
                  [clout "1.1.0"]
@@ -13,20 +14,29 @@
                  [lobos "1.0.0-beta1"]
                  [lib-noir "0.6.6"]]
   :ring {:handler reveriecms.dev/app}
-  :repl-options {:nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]}
   :profiles {:dev {:dependencies [[midje "1.6-alpha1"]
                                   [com.stuartsierra/lazytest "1.2.3"]
                                   [ring-mock "0.1.3"]
                                   [org.postgresql/postgresql "9.2-1002-jdbc4"]
+                                  [domina "1.0.1"]
+                                  [crate "0.2.4"]
+                                  [jayq "2.4.0"]
                                   [cljsbuild "0.3.2"]
+                                  [org.clojure/clojurescript "0.0-1853" :exclusions [[org.apache.ant/ant]]]
+                                  [org.clojure/google-closure-library-third-party "0.0-2029"]
                                   [org.clojure/tools.trace "0.7.5"]
-                                  [com.cemerick/piggieback "0.0.4"]]}}
+                                  [com.cemerick/piggieback "0.1.0"]
+                                  [org.clojure/core.async "0.1.0-SNAPSHOT"]]
+                   :plugins [[lein-cljsbuild "0.3.2"]]}}
+  :repl-options {:nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]}
   :cljsbuild {:builds {:prod {:source-paths ["src-cljs"]
                               :compiler {:output-to "resources/public/admin/js/main.js"
                                          :optimizations :advanced
-                                         :pretty-print false}}
+                                         :pretty-print false}
+                              :jar true}
                        :dev {:source-paths ["src-cljs"]
                              :compiler {:output-to "resources/public/admin/js/main-dev.js"
                                         :optimizations :whitespace
                                         :pretty-print true}}}}
-  :repositories {"stuart" "http://stuartsierra.com/maven2"})
+  :repositories {"stuart" "http://stuartsierra.com/maven2"
+                 "sonatype-oss-public" "https://oss.sonatype.org/content/groups/public/"})
