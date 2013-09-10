4c6d228576eb0f19becb5f68eedbcc14aaacc849
Emil Bengtsson <emil@emil0r.com>
2013-09-10 16:26:19 +0200

    reshuffling functions between file-picker and filemanager

--- src/reverie/admin/frames/file_picker.clj
+++ src/reverie/admin/frames/file_picker.clj
@@ -5,68 +5,14 @@
             [reverie.core :as rev]
             [reverie.responses :as r]
             [reverie.util :as util])
-  (:use [clj-time.coerce :only [from-long]]
-        [clj-time.format :only [unparse formatter]]
-        [hiccup core form]
+  (:use [hiccup core form]
         [reverie.admin.frames.common :only [frame-options]]
+        [reverie.admin.modules.filemanager :only [join-paths
+                                                  list-dir
+                                                  get-mod-time
+                                                  get-size]]
         [reverie.middleware :only [wrap-access]]))
 
-
-(defn- join-paths [& paths]
-  (s/join "/" (flatten (map (fn [p]
-                              (-> p
-                                  (s/replace #"\.\." "")
-                                  (s/split #"/"))) paths))))
-
-(defn- compare-listed [x y]
-  (cond
-   (and
-    (= (:type x) :directory)
-    (= (:type y) :directory)) (compare (:name x) (:name y))
-    (and
-    (= (:type x) :directory)
-    (= (:type y) :file)) -1
-    (and
-    (= (:type x) :file)
-    (= (:type y) :directory)) 1
-    :else (compare (:name x) (:name y)))
-  )
-
-(defn- list-dir [base path]
-  (let [base (-> fs/*cwd* (join-paths "media" (name base)))
-        listed (remove
-                #(re-find #"^\." %)
-                (-> base (join-paths path) fs/list-dir))]
-    (sort
-     compare-listed
-     (reduce (fn [out k]
-               (if (nil? k)
-                 out
-                 (let [p (join-paths base path k)]
-                   (conj out
-                         {:type (cond
-                                 (fs/directory? p) :directory
-                                 (fs/file? p) :file
-                                 :else :other)
-                          :mod (fs/mod-time p)
-                          :url (-> p
-                                   (s/replace (re-pattern
-                                               (-> p (s/split #"media") first)) "")
-                                   (s/replace #"media" "")
-                                   util/join-uri)
-                          :name (fs/name p)
-                          :size (fs/size p)}))))
-             []
-             listed))))
-
-(def time-display (formatter "YYYY-MM-dd HH:mm"))
-
-(defn- get-size [size]
-  (cond
-   (< 1 (/ size 1073741824)) (str (format "%.2f" (float (/ size 1073741824))) " GiB")
-   (< 1 (/ size 1048576)) (str (format "%.2f" (float (/ size 1048576))) " MiB")
-   (< 1 (/ size 1024)) (str (format "%.2f" (float (/ size 1024))) " KiB")
-   :else (str size " bytes")))
 
 (defmulti row-file :type)
 (defmethod row-file :directory [dir qs]
@@ -78,12 +24,12 @@
      [:i.icon-folder-close]
      (:name dir)]]
    [:td]
-   [:td (->> dir :mod from-long (unparse time-display))]])
+   [:td (get-mod-time dir)]])
 (defmethod row-file :file [file qs]
   [:tr
    [:td [:span.download {:url (:url file)} [:i.icon-download] (:name file)]]
-   [:td (-> file :size get-size)]
-   [:td (->> file :mod from-long (unparse time-display))]])
+   [:td (get-size file)]
+   [:td (get-mod-time file)]])
 (defmethod row-file :default [_ _])
 
 (defn- get-file-lister [files {:keys [qs up? path]}]
--- src/reverie/admin/modules/filemanager.clj
+++ src/reverie/admin/modules/filemanager.clj
@@ -1,5 +1,79 @@
 (ns reverie.admin.modules.filemanager
-  (:use [reverie.core :only [defmodule]]))
+  (:require [clojure.string :as s]
+            [me.raynes.fs :as fs]
+            [reverie.util :as util])
+  (:use [clj-time.coerce :only [from-long]]
+        [clj-time.format :only [unparse formatter]]
+        [reverie.core :only [defmodule]]
+        [hiccup core form]
+        [reverie.admin.frames.common :only [frame-options]]
+        [reverie.admin.templates :only [frame]]
+        [reverie.middleware :only [wrap-access]]))
 
 
-(defmodule filemanager {:name "File manager"})
+(def ^:private time-display (formatter "YYYY-MM-dd HH:mm"))
+
+(defn- compare-listed [x y]
+  (cond
+   (and
+    (= (:type x) :directory)
+    (= (:type y) :directory)) (compare (:name x) (:name y))
+    (and
+    (= (:type x) :directory)
+    (= (:type y) :file)) -1
+    (and
+    (= (:type x) :file)
+    (= (:type y) :directory)) 1
+    :else (compare (:name x) (:name y))))
+
+(defn join-paths [& paths]
+  (s/join "/" (flatten (map (fn [p]
+                              (-> p
+                                  (s/replace #"\.\." "")
+                                  (s/split #"/"))) paths))))
+
+(defn list-dir [base path]
+  (let [base (-> fs/*cwd* (join-paths "media" (name base)))
+        listed (remove
+                #(re-find #"^\." %)
+                (-> base (join-paths path) fs/list-dir))]
+    (sort
+     compare-listed
+     (reduce (fn [out k]
+               (if (nil? k)
+                 out
+                 (let [p (join-paths base path k)]
+                   (conj out
+                         {:type (cond
+                                 (fs/directory? p) :directory
+                                 (fs/file? p) :file
+                                 :else :other)
+                          :mod (fs/mod-time p)
+                          :url (-> p
+                                   (s/replace (re-pattern
+                                               (-> p (s/split #"media") first)) "")
+                                   (s/replace #"media" "")
+                                   util/join-uri)
+                          :name (fs/name p)
+                          :size (fs/size p)}))))
+             []
+             listed))))
+
+(defn get-mod-time [{:keys [mod]}]
+  (if mod
+    (->> mod from-long (unparse time-display))))
+
+(defn get-size [{:keys [size]}]
+  (cond
+   (< 1 (/ size 1073741824)) (str (format "%.2f" (float (/ size 1073741824))) " GiB")
+   (< 1 (/ size 1048576)) (str (format "%.2f" (float (/ size 1048576))) " MiB")
+   (< 1 (/ size 1024)) (str (format "%.2f" (float (/ size 1024))) " KiB")
+   :else (str size " bytes")))
+
+
+(defmodule filemanager {:name "File manager"
+                        :middleware [[wrap-access :edit]]}
+  [:get ["/"]
+   (frame
+    frame-options
+    "foo")])
