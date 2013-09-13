7d3ae5032cde83c9c8972b0611fb1da337206132
Emil Bengtsson <emil@emil0r.com>
2013-09-13 12:40:54 +0200

    updated crumb to take hashmap instead as last argument. added default argument for just the uri/uri-data

--- src/reverie/batteries/breadcrumbs.clj
+++ src/reverie/batteries/breadcrumbs.clj
@@ -6,9 +6,9 @@
   "Takes an URI or a sequence of vectors with [URI part, name of URI]"
   (fn [to-crumb & _] (class to-crumb)))
 (defmethod crumb java.lang.String
-  ([uri separator]
-     (crumb uri separator true))
-  ([uri separator last?]
+  ([uri]
+     (crumb uri {:separator " › " :last? true}))
+  ([uri {:keys [separator last?]}]
      (let [parts (remove s/blank? (s/split uri #"/"))
            crumbs (reduce (fn [out index]
                             (if (= index (count parts))
@@ -21,9 +21,9 @@
        {:crumbs (if last? crumbs (butlast crumbs))
         :last (last parts)})))
 (defmethod crumb clojure.lang.IPersistentVector
-  ([uri-data separator]
-     (crumb uri-data separator true))
-  ([uri-data separator last?]
+  ([uri-data]
+     (crumb uri-data {:separator " › " :last? true}))
+  ([uri-data {:keys [separator last?]}]
      (let [parts (remove (fn [[part _]] (s/blank? part)) uri-data)
            crumbs (reduce (fn [out index]
                             (if (= index (count parts))
@@ -36,9 +36,9 @@
        {:crumbs (if last? crumbs (butlast crumbs))
         :last (last parts)})))
 (defmethod crumb clojure.lang.IPersistentList
-  ([uri-data separator]
-     (crumb (vec uri-data) separator true))
-  ([uri-data separator last?]
-     (crumb (vec uri-data) separator last?)))
+  ([uri-data]
+     (crumb (vec uri-data) {:separator " › " :last? true}))
+  ([uri-data data]
+     (crumb (vec uri-data) data)))
 
 (defmethod crumb :default [_ _])
