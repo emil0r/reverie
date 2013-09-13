346320f90d7aede13354c2879357dc2e0a26941f
Emil Bengtsson <emil@emil0r.com>
2013-09-13 13:22:24 +0200

    updated breadcrumbs to give a hiccup vector along with raw data

--- src/reverie/batteries/breadcrumbs.clj
+++ src/reverie/batteries/breadcrumbs.clj
@@ -1,6 +1,22 @@
 (ns reverie.batteries.breadcrumbs
   (:require [clojure.string :as s]))
 
+(defn- get-crumbs [{:keys [crumb-data holder holder-attrib elem elem-attrib
+                           href-attrib separator last? parts]
+                    :or {separator " › " last? true holder :ul elem :li}}]
+  [holder holder-attrib
+   (butlast
+    (interleave
+     (map (fn [[uri name]]
+            [elem elem-attrib
+             [:a (merge href-attrib {:href uri})
+              name]]) crumb-data)
+     (map (fn [_] [:li.separator separator]) (range (count crumb-data)))))
+   (if (and last? (not (empty? crumb-data)))
+     [:li.separator separator])
+   (if last?
+     [elem elem-attrib
+      (if (sequential? (last parts)) (second (last parts)) (last parts))])])
 
 (defmulti crumb
   "Takes an URI or a sequence of vectors with [URI part, name of URI]"
@@ -8,32 +24,36 @@
 (defmethod crumb java.lang.String
   ([uri]
      (crumb uri {}))
-  ([uri {:keys [separator last?] :or {separator " › " last? true}}]
+  ([uri data]
      (let [parts (remove s/blank? (s/split uri #"/"))
            crumbs (reduce (fn [out index]
                             (if (= index (count parts))
                               out
                               (let [crumb (take (+ index 1) parts)]
                                 (conj out [(str "/" (s/join "/" crumb))
-                                           (s/join separator crumb)]))))
+                                           (last crumb)]))))
                           []
-                          (range (count parts)))]
-       {:crumbs (if last? crumbs (butlast crumbs))
+                          (range (count parts)))
+           crumb-data (butlast crumbs)]
+       {:crumb-data crumb-data
+        :crumbs (get-crumbs (merge data {:crumb-data crumb-data :parts parts}))
         :last (last parts)})))
 (defmethod crumb clojure.lang.IPersistentVector
   ([uri-data]
      (crumb uri-data {}))
-  ([uri-data {:keys [separator last?] :or {separator " › " last? true}}]
+  ([uri-data data]
      (let [parts (remove (fn [[part _]] (s/blank? part)) uri-data)
            crumbs (reduce (fn [out index]
                             (if (= index (count parts))
                               out
                               (let [crumb (take (+ index 1) parts)]
                                 (conj out [(str "/" (s/join "/" (map first crumb)))
-                                           (s/join separator (map second crumb))]))))
+                                           (second (nth crumb index))]))))
                           []
-                          (range (count parts)))]
-       {:crumbs (if last? crumbs (butlast crumbs))
+                          (range (count parts)))
+           crumb-data (butlast crumbs)]
+       {:crumb-data crumb-data
+        :crumbs (get-crumbs (merge data {:crumb-data crumb-data :parts parts}))
         :last (last parts)})))
 (defmethod crumb clojure.lang.IPersistentList
   ([uri-data]
