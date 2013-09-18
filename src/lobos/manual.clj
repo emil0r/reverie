9132816b97e36ea87754b1fdc6d0c8da093bb015
Emil Bengtsson <emil@emil0r.com>
2013-09-18 13:48:26 +0200

    moved migrations to its own specific namespace within reverie and added support for defining your own namespace for the migrations

--- src/lobos/manual.clj
+++ src/lobos/manual.clj
@@ -1,6 +1,6 @@
 (ns lobos.test.migrations
-  (require [lobos.core :as lobos]
-           [lobos.connectivity :as conn]))
+  (:require [lobos.core :as lobos]
+            [lobos.connectivity :as conn]))
 
 (def db {:classname "org.postgresql.Driver"
          :subprotocol "postgresql"
@@ -23,8 +23,7 @@
     ((lobos.connectivity/open-global db-spec) :default-connection)
     (@lobos.connectivity/global-connections :default-connection)))
 
-
 (open-global-when-necessary db)
 
-(lobos.core/rollback)
-(lobos.core/migrate)
+;;(lobos.core/rollback :all)
+;;(lobos.core/migrate)
