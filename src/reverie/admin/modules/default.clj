a09610f039b43fa3d5470fab19d18b4b4b76f7fd
Emil Bengtsson <emil@emil0r.com>
2013-11-17 23:47:27 +0100

    bugfix: valid-datetime?

--- src/reverie/admin/modules/default.clj
+++ src/reverie/admin/modules/default.clj
@@ -29,8 +29,10 @@
    :else :save))
 
 (defn- valid-datetime? [value]
-  (or (s/blank? value)
-      (re-find #"^\d{4,4}-\d{2,2}-\d{2,2} [0-2][0-9](?::[0-6][0-9]){1,2}$" (s/trim value))))
+  (cond
+   (instance? java.sql.Date value) true
+   (s/blank? value) true
+   :else (re-find #"^\d{4,4}-\d{2,2}-\d{2,2} [0-2][0-9](?::[0-6][0-9]){1,2}$" (s/trim value))))
 
 (defn- string->timestamp [timestamp]
   (if (valid-datetime? timestamp)
