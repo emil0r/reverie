2e61ddfda93a6a953b9bc5635428fd750f75d199
Emil Bengtsson <emil@emil0r.com>
2013-11-20 16:16:35 +0100

    updated settings to handle objects, apps and pages as well

--- src/reverie/settings.clj
+++ src/reverie/settings.clj
@@ -1,14 +1,45 @@
 (ns reverie.settings
   "Namespace for easier handling of reverie settings. Can be directly accessed through @reverie.atoms/settings as well"
   (:refer-clojure :exclude [read write])
-  (:use [reverie.atoms :only [settings]]))
+  (:use [reverie.atoms :only [settings objects pages apps]]))
 
 
-(defn read [& path]
+(defn read
+  "Read from @settings in reverie.atoms"
+  [& path]
   (get-in @settings path))
 
-(defn write! [& path]
+(defn write!
+  "Write to @settings in reverie.atoms"
+  [& path]
   (swap! settings assoc-in (butlast path) (last path)))
 
-(defn delete! [& path]
+(defn delete!
+  "Delete from @settings in reverie.atoms"
+  [& path]
   (swap! settings update-in (butlast path) dissoc (last path)))
+
+
+(defn options-read
+  "Read options in objects, pages or apps [:object :name-of-object :path :to :value]"
+  [what which & path]
+  (case what
+    :object (get-in @objects (flatten [which :options path]))
+    :page (get-in @pages (flatten [which :options path]))
+    :app (get-in @apps (flatten [which :options path]))))
+
+(defn options-write!
+  "Write options to objects, pages or apps [:object :name-of-object :path :to :value]"
+  [what which & path]
+  (case what
+    :object (swap! objects assoc-in (flatten [which :options (butlast path)]) (last path))
+    :page (swap! pages assoc-in (flatten [which :options (butlast path)]) (last path))
+    :app (swap! apps assoc-in (flatten [which :options (butlast path)]) (last path))))
+
+(defn options-delete!
+  "Delete options from objects, pages or apps [:object :name-of-object :path :to :value]"
+  [what which & path]
+  (case what
+    :object (swap! objects update-in (remove nil? (flatten [which :options (butlast path)])) dissoc (last path))
+    :page (swap! pages update-in (remove nil? (flatten [which :options (butlast path)])) dissoc (last path))
+    :app (swap! apps update-in (remove nil? (flatten [which :options (butlast path)])) dissoc (last path))))
