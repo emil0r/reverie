cac282590124b05819b46cf193b9c724ebb8f0ed
Emil Bengtsson <emil@emil0r.com>
2013-11-21 10:13:29 +0100

    path is now expected to be a vector. can now have default values

--- src/reverie/settings.clj
+++ src/reverie/settings.clj
@@ -1,48 +1,48 @@
 (ns reverie.settings
   "Namespace for easier handling of reverie settings. Can be directly accessed through @reverie.atoms/settings as well"
-  (:refer-clojure :exclude [read write])
+  (:refer-clojure :exclude [read])
   (:use [reverie.atoms :only [settings objects pages apps templates modules]]))
 
 
 (defn read
   "Read from @settings in reverie.atoms"
-  [& path]
-  (get-in @settings path))
+  [path & [default]]
+  (get-in @settings path default))
 
 (defn write!
   "Write to @settings in reverie.atoms"
-  [& path]
-  (swap! settings assoc-in (butlast path) (last path)))
+  [path value]
+  (swap! settings assoc-in path value))
 
 (defn delete!
   "Delete from @settings in reverie.atoms"
-  [& path]
+  [path]
   (swap! settings update-in (butlast path) dissoc (last path)))
 
 
 (defn options-read
   "Read options in objects, pages or apps [:object :name-of-object :path :to :value]"
-  [what which & path]
+  [what which path & [default]]
   (case what
-    :object (get-in @objects (flatten [which :options path]))
-    :page (get-in @pages (flatten [which :options path]))
-    :app (get-in @apps (flatten [which :options path]))
-    :template (get-in @templates (flatten [which :options path]))
-    :module (get-in @modules (flatten [which :options path]))))
+    :object (get-in @objects (flatten [which :options path]) default)
+    :page (get-in @pages (flatten [which :options path]) default)
+    :app (get-in @apps (flatten [which :options path]) default)
+    :template (get-in @templates (flatten [which :options path]) default)
+    :module (get-in @modules (flatten [which :options path]) default)))
 
 (defn options-write!
   "Write options to objects, pages or apps [:object :name-of-object :path :to :value]"
-  [what which & path]
+  [what which path value]
   (case what
-    :object (swap! objects assoc-in (flatten [which :options (butlast path)]) (last path))
-    :page (swap! pages assoc-in (flatten [which :options (butlast path)]) (last path))
-    :app (swap! apps assoc-in (flatten [which :options (butlast path)]) (last path))
-    :template (swap! templates assoc-in (flatten [which :options (butlast path)]) (last path))
-    :module (swap! modules assoc-in (flatten [which :options (butlast path)]) (last path))))
+    :object (swap! objects assoc-in (flatten [which :options path]) value)
+    :page (swap! pages assoc-in (flatten [which :options path]) value)
+    :app (swap! apps assoc-in (flatten [which :options path]) value)
+    :template (swap! templates assoc-in (flatten [which :options path]) value)
+    :module (swap! modules assoc-in (flatten [which :options path]) value)))
 
 (defn options-delete!
   "Delete options from objects, pages or apps [:object :name-of-object :path :to :value]"
-  [what which & path]
+  [what which path]
   (case what
     :object (swap! objects update-in (remove nil? (flatten [which :options (butlast path)])) dissoc (last path))
     :page (swap! pages update-in (remove nil? (flatten [which :options (butlast path)])) dissoc (last path))
