e73c3a41e10f3032e160196ead7d0b11751f9f0f
Emil Bengtsson <emil@emil0r.com>
2014-12-23 13:12:55 +0200

    updated deps

--- reverie-core/project.clj
+++ reverie-core/project.clj
@@ -4,6 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
+                 [ring/ring-core "1.3.1"]
                  [clout "2.0.0"]
                  [slingshot "0.12.1"]
                  [com.stuartsierra/component "0.2.2"]]
