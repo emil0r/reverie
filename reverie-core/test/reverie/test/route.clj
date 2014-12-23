57ecf3ea7aab0890b0358cbd9b7c792cc5860fce
Emil Bengtsson <emil@emil0r.com>
2014-12-23 13:13:45 +0200

    updated test suite to route

--- reverie-core/test/reverie/test/route.clj
+++ reverie-core/test/reverie/test/route.clj
@@ -11,11 +11,16 @@
                r (assoc (request :get "/foo/1234" {:foo "bar"})
                    :params {:foo "bar"}))
        match2 (route/match?
-               r (request :get "/"))]
-   (fact "match1 has been a hit"
+               r (request :get "/"))
+       match3 (route/match?
+               (route/route ["/foo/:bar"]) (request :get "/foo/1234"))]
+   (fact "match1: hit"
          (nil? match1) => false)
-   (fact "bar has been casted to an integer"
+   (fact "match1: bar has been casted to an integer"
          (get-in match1 [:request :params :bar])
          => 1234)
-   (fact "match2 has not been hit"
-         (nil? match2) => true)))
+   (fact "match2: no hit"
+         (nil? match2) => true)
+   (fact "match3: bar has been caught"
+         (get-in match3 [:request :params :bar])
+         => "1234")))
