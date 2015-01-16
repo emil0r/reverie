8f0991d1fdcc45c72f2d25ecdbef27e2d2919c63
Emil Bengtsson <emil@emil0r.com>
2015-01-16 09:51:08 +0200

    bit more for batteries

--- reverie-batteries/project.clj
+++ reverie-batteries/project.clj
@@ -3,4 +3,5 @@
   :url "http://www.reveriecms.org/batteries"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]])
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [ez-image "1.0.2"]])
---
+++ reverie-batteries/src/reverie/sql/objects/image.clj
@@ -0,0 +1,40 @@
+(ns reverie.sql.objects.image
+  (:require [ez-image.core :as img]
+            [reverie.core :refer [defobject]]))
+
+(defn- image [request object {:keys [title alt src width height]} params]
+  (let [[width height] (cond
+                        (and width height) [width height]
+                        width [width 99999]
+                        height [99999 height]
+                        :else [nil nil])]
+    (if-not (or width height)
+      [:img {:src src :title title :alt alt}]
+      [:img {:src (img/cache src [:constrain width height])
+             :title title :alt alt}])))
+
+
+
+(defobject reverie/image
+  {:name :image
+   :table "batteries_image"
+   :migration {:path "src/reverie/sql/objects/migrations/image/"
+               :automatic? true}
+   :properties-order [:title :alt :src :width :height]
+   :properties {:title {:name "Title"
+                        :input :text
+                        :initial ""
+                        :max 100}
+                :alt {:name "Alt"
+                      :input :text
+                      :initial ""
+                      :max 100}
+                :src {:name "src"
+                      :input :image
+                      :initial ""
+                      :max 255}
+                :height {:name "Height"
+                         :input :number}
+                :width {:name "Width"
+                        :input :number}}}
+  {:any image})
