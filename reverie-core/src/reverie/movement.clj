1e3dc1efe8a3e6c393abc2d3c682e2579e0deeb0
Emil Bengtsson <emil@emil0r.com>
2015-01-17 20:30:22 +0200

    movement of order added (with and without a origo)

---
+++ reverie-core/src/reverie/movement.clj
@@ -0,0 +1,155 @@
+(ns reverie.movement
+  (:require [clojure.zip :as zip]))
+
+
+(defn- beginning? [loc]
+  (= (zip/node loc) (zip/node (zip/leftmost loc))))
+(defn- end? [loc]
+  (= (zip/node loc) (zip/node (zip/rightmost loc))))
+
+(defn insert-origo [ids]
+  (if (empty? ids)
+    [[0 nil]]
+    (loop [loc1 (zip/next (zip/vector-zip ids))
+           val1 (zip/node loc1)]
+      (let [loc2 (if (-> loc1 zip/next zip/next zip/end?)
+                   nil
+                   (-> loc1 zip/right))
+            val2 (if (nil? loc2)
+                   nil
+                   (zip/node loc2))]
+
+        (cond
+         (zero? (first val1)) (zip/root loc1)
+         (and (neg? (first val1)) (nil? val2))
+         (-> loc1
+             (zip/insert-right [0 nil])
+             zip/root)
+         (and (pos? (first val1)) (nil? val2))
+         (-> loc1
+             (zip/insert-left [0 nil])
+             zip/root)
+         (and (neg? (first val1)) (pos? (first val2)))
+         (-> loc1
+             (zip/insert-right [0 nil])
+             zip/root)
+         (and (pos? (first val1)) (pos? (first val2)))
+         (-> loc1
+             (zip/insert-left [0 nil])
+             zip/root)
+         (zip/end? loc1) (zip/root loc1)
+         :else (recur (zip/right loc1) (zip/node (zip/right loc1))))))))
+
+(defn- dir-up [ids id]
+  (loop [loc (zip/vector-zip ids)]
+    (let [next-loc (zip/next loc)]
+      (if (zip/end? next-loc)
+        (zip/root loc)
+        (let [node-value (zip/node next-loc)]
+          (if (= id node-value)
+            (if (beginning? next-loc)
+              (zip/root next-loc)
+              (-> next-loc
+                  zip/remove
+                  (zip/insert-left node-value)
+                  zip/root))
+            (recur next-loc)))))))
+
+(defn- dir-down [ids id]
+  (loop [loc (zip/vector-zip ids)]
+    (let [next-loc (zip/next loc)]
+      (if (zip/end? next-loc)
+        (zip/root loc)
+        (let [node-value (zip/node next-loc)]
+          (if (= id node-value)
+            (if (end? next-loc)
+              (zip/root next-loc)
+              (-> next-loc
+                  zip/remove
+                  zip/next
+                  (zip/insert-right node-value)
+                  zip/root))
+            (recur next-loc)))))))
+
+(defn- dir-top [ids id]
+  (loop [loc (zip/vector-zip ids)]
+    (let [next-loc (zip/next loc)]
+      (if (zip/end? next-loc)
+        (zip/root loc)
+        (let [node-value (zip/node next-loc)]
+          (if (= id node-value)
+            (if (beginning? next-loc)
+              (zip/root next-loc)
+              (-> next-loc
+                  zip/remove
+                  zip/leftmost
+                  (zip/insert-left node-value)
+                  zip/root))
+            (recur next-loc)))))))
+
+(defn- dir-bottom [ids id]
+  (loop [loc (zip/vector-zip ids)]
+    (let [next-loc (zip/next loc)]
+      (if (zip/end? next-loc)
+        (zip/root loc)
+        (let [node-value (zip/node next-loc)]
+          (if (= id node-value)
+            (do
+              (if (end? next-loc)
+                (zip/root next-loc)
+                (-> next-loc
+                    zip/remove
+                    zip/next
+                    zip/rightmost
+                    (zip/insert-right node-value)
+                    zip/root)))
+            (recur next-loc)))))))
+
+(defn- get-zero-index [ids]
+  (->> ids
+       (map-indexed (fn [index id] [index id]))
+       (filter (fn [[index id]] (nil? id)))
+       ffirst))
+
+(defn- re-index [ids origo?]
+  (if origo?
+    (let [z (get-zero-index ids)]
+      (vec (map-indexed (fn [index id]
+                          [(- index z) id]) ids)))
+    (vec (map-indexed (fn [index id]
+                        [(inc index) id]) ids))))
+
+(defn move "ids -> [[order id] [order id] ...]"
+  ([ids id direction]
+     (move! ids id direction false))
+  ([ids id direction origo?]
+     (let [ids (vec (map second (if origo?
+                                      (insert-origo ids)
+                                      ids)))
+           ids-ordered
+           (case (keyword direction)
+             :up (dir-up ids id)
+             :down (dir-down ids id)
+             :top (dir-top ids id)
+             :bottom (dir-bottom ids id))]
+       (re-index ids-ordered origo?))))
+
+(defn after "ids -> [[order id] [order id] ...]"
+  ([ids id new-id]
+     (after ids id new-id false))
+  ([ids id new-id origo?]
+     (let [ids (vec (map second (if origo?
+                                  (insert-origo ids)
+                                  ids)))]
+       (loop [loc (zip/vector-zip ids)]
+         (let [next-loc (zip/next loc)]
+           (if (zip/end? next-loc)
+             (re-index
+              (zip/root (zip/append-child (zip/vector-zip ids) new-id))
+              origo?)
+             (let [node-value (zip/node next-loc)]
+               (if (= node-value id)
+                 (re-index
+                  (zip/root (zip/insert-right next-loc new-id))
+                  origo?)
+                 (recur next-loc)))))))))
---
+++ reverie-core/test/reverie/test/movement.clj
@@ -0,0 +1,77 @@
+(ns reverie.test.movement
+  (:require [reverie.movement :as m]
+            [midje.sweet :refer :all]))
+
+
+(fact
+ "insert origo"
+ (fact "empty"
+       (m/insert-origo [])
+       => [[0 nil]])
+ (fact "[[-1 1]]"
+       (m/insert-origo [[-1 1]])
+       => [[-1 1] [0 nil]])
+ (fact "[[1 1]]"
+       (m/insert-origo [[1 1]])
+       => [[0 nil] [1 1]])
+ (fact "[[-2 2] [-1 1]]"
+       (m/insert-origo [[-2 2] [-1 1]])
+       => [[-2 2] [-1 1] [0 nil]])
+ (fact "[[1 1] [2 2]]"
+       (m/insert-origo [[1 1] [2 2]])
+       => [[0 nil] [1 1] [2 2]])
+ (fact "[[-1 1] [1 2] [2 3]]"
+       (m/insert-origo [[-1 1] [1 2] [2 3]])
+       => [[-1 1] [0 nil] [1 2] [2 3]])
+ (fact "[[-2 1] [-1 2] [1 3] [2 4]]"
+       (m/insert-origo [[-2 1] [-1 2] [1 3] [2 4]])
+       => [[-2 1] [-1 2] [0 nil] [1 3] [2 4]])
+ (fact "[[-2 1] [-1 2] [1 3]]"
+       (m/insert-origo [[-2 1] [-1 2] [1 3]])
+       => [[-2 1] [-1 2] [0 nil] [1 3]])
+ (fact "[[-2 1] [2 2]]"
+       (m/insert-origo [[-2 1] [2 2]])
+       => [[-2 1] [0 nil] [2 2]]))
+
+
+
+(fact "move"
+      (fact "down simple (3)"
+            (m/move [[1 1] [2 2] [3 3] [4 4]] 3 :down)
+            => [[1 1] [2 2] [3 4] [4 3]])
+      (fact "up simple (3)"
+            (m/move [[1 1] [2 2] [3 3] [4 4]] 3 :up)
+            => [[1 1] [2 3] [3 2] [4 4]])
+      (fact "top simple (3)"
+            (m/move [[1 1] [2 2] [3 3] [4 4]] 3 :top)
+            => [[1 3] [2 1] [3 2] [4 4]])
+      (fact "bottom simple (1)"
+            (m/move [[1 1] [2 2] [3 3] [4 4]] 1 :bottom)
+            => [[1 2] [2 3] [3 4] [4 1]])
+      (fact "down (3)"
+            (m/move [[-2 6] [-1 5] [1 1] [2 2] [3 3] [4 4]] 3 :down true)
+            => [[-2 6] [-1 5] [0 nil] [1 1] [2 2] [3 4] [4 3]])
+      (fact "up (3)"
+            (m/move [[-2 6] [-1 5] [1 1] [2 2] [3 3] [4 4]] 3 :up true)
+            => [[-2 6] [-1 5] [0 nil] [1 1] [2 3] [3 2] [4 4]])
+      (fact "up (1)"
+            (m/move [[-2 6] [-1 5] [1 1] [2 2] [3 3] [4 4]] 1 :up true)
+            => [[-3 6] [-2 5] [-1 1] [0 nil] [1 2] [2 3] [3 4]])
+      (fact "up (1), zero already there"
+            (m/move [[-2 6] [-1 5] [0 nil] [1 1] [2 2] [3 3] [4 4]] 1 :up true)
+            => [[-3 6] [-2 5] [-1 1] [0 nil] [1 2] [2 3] [3 4]])
+      (fact "top (3)"
+            (m/move [[-2 6] [-1 5] [1 1] [2 2] [3 3] [4 4]] 3 :top true)
+            => [[-3 3] [-2 6] [-1 5] [0 nil] [1 1] [2 2] [3 4]])
+      (fact "bottom (1)"
+            (m/move [[-1 5] [1 1] [2 2] [3 3] [4 4]] 1 :bottom true)
+            => [[-1 5] [0 nil] [1 2] [2 3] [3 4] [4 1]]))
+
+
+(fact "after"
+      (fact "no origo"
+            (m/after [[1 20] [2 40] [3 24]] 40 5)
+            => [[1 20] [2 40] [3 5] [4 24]])
+      (fact "with origo"
+            (m/after [[-2 31] [-1 3] [1 20] [2 40] [3 24]] 3 5 true)
+            => [[-3 31] [-2 3] [-1 5] [0 nil] [1 20] [2 40] [3 24]]))
