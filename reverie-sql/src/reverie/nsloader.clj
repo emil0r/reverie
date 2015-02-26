f47b85d0bcfe9a5db3f02a95b8babb32e7ae5ceb
Emil Bengtsson <emil@emil0r.com>
2015-02-26 12:37:38 +0200

    add loader for namespaces

---
+++ reverie-sql/src/reverie/nsloader.clj
@@ -0,0 +1,6 @@
+(ns reverie.nsloader
+  (:require reverie.database
+            reverie.database.sql
+            reverie.module
+            reverie.module.entity
+            reverie.modules.sql))
