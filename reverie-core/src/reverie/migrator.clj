4879f3d1c0b0ac71b2bcafea4d122c846a66f3be
Emil Bengtsson <emil@emil0r.com>
2016-02-19 12:23:02 +0100

    can now manually add and remove migrations

--- reverie-core/src/reverie/migrator.clj
+++ reverie-core/src/reverie/migrator.clj
@@ -1,4 +1,12 @@
-(ns reverie.migrator)
+(ns reverie.migrator
+  (:require [reverie.system :as sys]))
 
 (defprotocol IMigrator
   (migrate [migrator]))
+
+
+(defn add-migration! [name migration]
+  (swap! sys/storage assoc-in [:migrations name] migration))
+
+(defn remove-migration! [name]
+  (swap! sys/storage update-in [:migrations] dissoc name))
