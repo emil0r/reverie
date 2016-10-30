bb0a2f3d10b917307da9f916a58dff6a9ddd4d5b
Emil Bengtsson <emil@emil0r.com>
2016-10-30 20:49:10 +0200

    reverie/raw in reverie-batteries is a little bit less dangerous

--- reverie-batteries/src/reverie/sql/objects/raw.clj
+++ reverie-batteries/src/reverie/sql/objects/raw.clj
@@ -1,9 +1,14 @@
 (ns reverie.sql.objects.raw
-  (:require [reverie.core :refer [defobject]]))
+  (:require [hiccup.util :refer [escape-html]]
+            [reverie.core :refer [defobject]]))
 
 
-(defn raw [request object {:keys [text] :as properties} params]
-  text)
+(defn raw [{{edit? :edit?} :reverie} object {:keys [text]} params]
+  ;; due to raw being so dangerous, we escape the text when in edit? mode
+  ;; in case there is content in there that breaks the site
+  (if edit?
+    (escape-html text)
+    text))
 
 (defobject reverie/raw
   {:table "batteries_raw"
