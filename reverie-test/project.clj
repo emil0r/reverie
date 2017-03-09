b838cf6cfd874d2b7e80ac20e293ff09870ea55c
Emil Bengtsson <emil@emil0r.com>
2017-03-09 17:24:32 +0100

    version 0.8.1

--- reverie-test/project.clj
+++ reverie-test/project.clj
@@ -4,11 +4,11 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [reverie-core "0.8.1-SNAPSHOT"]
-                 [reverie-sql "0.8.1-SNAPSHOT"]
+                 [reverie-core "0.8.1"]
+                 [reverie-sql "0.8.1"]
                  [reverie-batteries "0.4.0"]
                  [reverie-redis "0.1.0"]
-                 [reverie-blog "0.3.3-SNAPSHOT"]
+                 [reverie-blog "0.3.4"]
                  [reverie-blockade "0.11.0"]
                  [http-kit "2.1.19"]
                  [org.postgresql/postgresql "9.3-1102-jdbc41"]]
