b4c70cb5c47607b1d89201b515510577562eb56e
Emil Bengtsson <emil@emil0r.com>
2019-11-11 21:44:16 +0100

    0.9.0-alpha3

--- reverie-sql/project.clj
+++ reverie-sql/project.clj
@@ -1,4 +1,4 @@
-(defproject reverie-sql "0.9.0-SNAPSHOT"
+(defproject reverie-sql "0.9.0-alpha3"
   :description "The SQL backbone of reverie; a CMS for power users"
   :url "http://reveriecms.org"
   :license {:name "Eclipse Public License"
@@ -7,7 +7,7 @@
                  [org.clojure/clojure "1.10.1"]
 
                  ;; reverie
-                 [reverie-core "0.9.0-SNAPSHOT"]
+                 [reverie-core "0.9.0-alpha3"]
 
                  ;; database libraries
                  [org.clojure/java.jdbc "0.7.8"]
