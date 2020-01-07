506677451ccc2c353d479a1f595b15c8b8d701f6
Emil Bengtsson <emil@emil0r.com>
2020-01-08 00:43:00 +0100

    added some casts from Prismatic Schema to reverie.cast

--- reverie-core/src/reverie/cast.clj
+++ reverie-core/src/reverie/cast.clj
@@ -1,22 +1,34 @@
 (ns reverie.cast
+  (:require [schema.core :as s])
   (:refer-clojure :exclude [cast]))
 
 (defprotocol ICast
   (cast [to from-str]))
 
 (defmulti class->cast (fn [to _] to))
-(defmethod class->cast "java.lang.Integer" [_ from-str]
+(defmethod class->cast java.lang.Integer [_ from-str]
   (Integer/parseInt from-str))
-(defmethod class->cast "java.lang.Float" [_ from-str]
+(defmethod class->cast java.lang.Float [_ from-str]
   (Float/parseFloat from-str))
-(defmethod class->cast "java.lang.Double" [_ from-str]
+(defmethod class->cast java.lang.Double [_ from-str]
   (Double/parseDouble from-str))
-(defmethod class->cast "java.lang.Long" [_ from-str]
+(defmethod class->cast java.lang.Long [_ from-str]
   (Long/parseLong from-str))
+;; strictly speaking this could hold any type of number,
+;; but by convention s/Num is for irrational numbers
+(defmethod class->cast java.lang.Number [_ from-str]
+  (Double/parseDouble from-str))
+(defmethod class->cast s/Int [_ from-str]
+  (Long/parseLong from-str))
+(defmethod class->cast s/Keyword [_ from-str]
+  (keyword from-str))
 (defmethod class->cast :default [_ from-str]
   from-str)
 
 (extend-protocol ICast
   java.lang.Class
   (cast [to from-str]
-    (class->cast (.getName to) from-str)))
+    (class->cast to from-str))
+  schema.core.Predicate
+  (cast [to from-str]
+    (class->cast to from-str)))
